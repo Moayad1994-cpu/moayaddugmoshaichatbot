dd70934a1fe125cbaabf8f130f1bb2e4965a9b68
Moayad1994-cpu <173873078+Moayad1994-cpu@users.noreply.github.com>
2025-01-22 11:37:08 +0400

    app.py

--- app.py
+++ app.py
@@ -67,4 +67,5 @@ def get_conversation_log():
     return jsonify(conversation_log)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
